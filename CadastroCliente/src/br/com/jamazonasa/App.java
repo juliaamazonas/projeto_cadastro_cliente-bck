08a6402911ef218a4ff65b5dd495be5576a5d76e
Julia Amazonas <juliaamazonasa@gmail.com>
2024-11-21 23:00:28 -0300

    iniciando o projeto

---
+++ CadastroCliente/src/br/com/jamazonasa/App.java
@@ -0,0 +1,20 @@
+package br.com.jamazonasa;
+
+import br.com.jamazonasa.dao.ClienteMapDAO;
+import br.com.jamazonasa.dao.IClienteDAO;
+
+import javax.swing.*;
+
+public class App {
+
+    private static IClienteDAO iClienteDAO;
+
+    public static void main(String args[]) {
+        iClienteDAO = new ClienteMapDAO();
+
+        String opcao = JOptionPane.showInputDialog(null,
+                "Digite 1 para cadastro, 2 para consultar, 3 para exclusão, 4 para alteração ou 5 para sair",
+                "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+}
---
+++ CadastroCliente/src/br/com/jamazonasa/dao/IClienteDAO.java
@@ -0,0 +1,16 @@
+package br.com.jamazonasa.dao;
+
+import br.com.jamazonasa.domain.Cliente;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
